9a8e7758ee2cdcd9d3d276710e0339f0888ba18b
Traun Leyden <traun.leyden@gmail.com>
2017-08-01 08:48:56 -0700

    Update fetch_aws_keys to support multiple aws accounts

--- fetch_aws_keys.go
+++ fetch_aws_keys.go
@@ -11,7 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/tleyden/keynuker/keynuker-go-common"
-	"log"
+	"time"
 )
 
 // Look up all the AWS keys associated with the AWS account corresponding to AwsAccessKeyId
@@ -30,55 +30,61 @@ func FetchAwsKeys(params ParamsFetchAwsKeys) (docWrapper DocumentWrapperFetchAws
 	// Create output document + wrapepr
 	doc := DocumentFetchAwsKeys{
 		Id:                docId,
-		AwsAccessKeyId:    params.AwsAccessKeyId,
-		AccessKeyMetadata: []iam.AccessKeyMetadata{},
+		AccessKeyMetadata: []FetchedAwsAccessKey{},
 	}
 
-	log.Printf("create AWS session with params: %+v", params)
+	for _, targetAwsAccount := range params.TargetAwsAccounts {
 
-	// Create AWS session
-	sess, err := session.NewSession(&aws.Config{
-		Credentials: credentials.NewCredentials(
-			&credentials.StaticProvider{Value: credentials.Value{
-				AccessKeyID:     params.AwsAccessKeyId,
-				SecretAccessKey: params.AwsSecretAccessKey,
-			}},
-		),
-	})
-	if err != nil {
-		return DocumentWrapperFetchAwsKeys{}, fmt.Errorf("Error creating aws session: %v", err)
-	}
-
-	// Create IAM client with the session.
-	svc := iam.New(sess)
-
-	// Fetch list of IAM users
-	iamUsers, err := FetchIAMUsers(svc)
-	if err != nil {
-		return DocumentWrapperFetchAwsKeys{}, err
-	}
-
-	for _, user := range iamUsers {
-
-		listAccessKeysInput := &iam.ListAccessKeysInput{
-			UserName: user.UserName,
-			MaxItems: aws.Int64(1000),
-		}
-
-		listAccessKeysOutput, err := svc.ListAccessKeys(listAccessKeysInput)
+		// Create AWS session
+		sess, err := session.NewSession(&aws.Config{
+			Credentials: credentials.NewCredentials(
+				&credentials.StaticProvider{Value: credentials.Value{
+					AccessKeyID:     targetAwsAccount.AwsAccessKeyId,
+					SecretAccessKey: targetAwsAccount.AwsSecretAccessKey,
+				}},
+			),
+		})
 		if err != nil {
-			return DocumentWrapperFetchAwsKeys{}, fmt.Errorf("Error listing access keys for user: %v.  Err: %v", user, err)
+			return DocumentWrapperFetchAwsKeys{}, fmt.Errorf("Error creating aws session: %v", err)
 		}
 
-		// Panic if more than 1K results, which is not handled
-		if *listAccessKeysOutput.IsTruncated {
-			// TODO: remove panic and put in a paginated loop.  Move to tleyden/awsutils + unit tests against mocks
-			return DocumentWrapperFetchAwsKeys{}, fmt.Errorf("Output is truncated and this code does not handle it")
+		// Create IAM client with the session.
+		svc := iam.New(sess)
+
+		// Fetch list of IAM users
+		iamUsers, err := FetchIAMUsers(svc)
+		if err != nil {
+			return DocumentWrapperFetchAwsKeys{}, err
 		}
 
-		// doc.AccessKeyMetadata = append(doc.AccessKeyMetadata, listAccessKeysOutput.AccessKeyMetadata...)
-		for _, accessKeyMetadata := range listAccessKeysOutput.AccessKeyMetadata {
-			doc.AccessKeyMetadata = append(doc.AccessKeyMetadata, *accessKeyMetadata)
+		for _, user := range iamUsers {
+
+			listAccessKeysInput := &iam.ListAccessKeysInput{
+				UserName: user.UserName,
+				MaxItems: aws.Int64(1000),
+			}
+
+			listAccessKeysOutput, err := svc.ListAccessKeys(listAccessKeysInput)
+			if err != nil {
+				return DocumentWrapperFetchAwsKeys{}, fmt.Errorf("Error listing access keys for user: %v.  Err: %v", user, err)
+			}
+
+			// Panic if more than 1K results, which is not handled
+			if *listAccessKeysOutput.IsTruncated {
+				// TODO: remove panic and put in a paginated loop.  Move to tleyden/awsutils + unit tests against mocks
+				return DocumentWrapperFetchAwsKeys{}, fmt.Errorf("Output is truncated and this code does not handle it")
+			}
+
+			for _, accessKeyMetadata := range listAccessKeysOutput.AccessKeyMetadata {
+
+				fetchedAwsAccessKey := NewFetchedAwsAccessKey(
+					accessKeyMetadata,
+					targetAwsAccount.AwsAccessKeyId,
+				)
+
+				doc.AccessKeyMetadata = append(doc.AccessKeyMetadata, *fetchedAwsAccessKey)
+			}
+
 		}
 
 	}
@@ -112,7 +118,7 @@ func FetchIAMUsers(svc *iam.IAM) (users []*iam.User, err error) {
 
 }
 
-type ParamsFetchAwsKeys struct {
+type TargetAwsAccount struct {
 
 	// The aws access key to connect as.  This only needs permissions to list IAM users and access keys,
 	// and delete access keys (in the case they are nuked)
@@ -120,16 +126,53 @@ type ParamsFetchAwsKeys struct {
 
 	// The secret access key corresponding to AwsAccessKeyId
 	AwsSecretAccessKey string
+}
+
+type ParamsFetchAwsKeys struct {
+
+	// The list of AWS accounts to fetch all the access keys for
+	TargetAwsAccounts []TargetAwsAccount
 
 	// This is the name of the KeyNuker "org/tenant".  Defaults to "default", but allows to be extended multi-tenant.
 	KeyNukerOrg string
 }
 
+// This encapsulates all of the fields from iam.AccessKeyMetadata, as well as addding the FetcherAwsAccessKeyId
+// that was used to fetch (and should be used to nuke key if needed)
+type FetchedAwsAccessKey struct {
+
+	// The ID for this access key.
+	AccessKeyId *string `min:"16" type:"string"`
+
+	// The date when the access key was created.
+	CreateDate *time.Time `type:"timestamp" timestampFormat:"iso8601"`
+
+	// The status of the access key. Active means the key is valid for API calls;
+	// Inactive means it is not.
+	Status *string `type:"string" enum:"statusType"`
+
+	// The name of the IAM user that the key is associated with.
+	UserName *string `min:"1" type:"string"`
+
+	// The AWS access key used to fetch.  Need to track since this same key will need to be used to nuke as well
+	FetcherAwsAccessKeyId string
+}
+
+// Create a new FetchedAwsAccessKey
+func NewFetchedAwsAccessKey(accessKeyMetadata *iam.AccessKeyMetadata, fetcherAwsAccessKeyId string) *FetchedAwsAccessKey {
+	return &FetchedAwsAccessKey{
+		AccessKeyId:           accessKeyMetadata.AccessKeyId,
+		CreateDate:            accessKeyMetadata.CreateDate,
+		Status:                accessKeyMetadata.Status,
+		UserName:              accessKeyMetadata.UserName,
+		FetcherAwsAccessKeyId: fetcherAwsAccessKeyId,
+	}
+}
+
 type DocumentFetchAwsKeys struct {
-	Id                string `json:"_id"`
-	AwsAccountId      string
-	AwsAccessKeyId    string
-	AccessKeyMetadata []iam.AccessKeyMetadata
+	Id string `json:"_id"`
+
+	AccessKeyMetadata []FetchedAwsAccessKey
 }
 
 type DocumentWrapperFetchAwsKeys struct {
---
+++ fetch_aws_keys_test.go
@@ -0,0 +1,44 @@
+package keynuker
+
+import (
+	"testing"
+	"os"
+	"github.com/tleyden/keynuker/keynuker-go-common"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"encoding/json"
+)
+
+func TestFetchAwsKeys(t *testing.T) {
+
+	awsAccessKeyId, ok := os.LookupEnv(keynuker_go_common.EnvVarKeyNukerTestAwsAccessKeyId)
+	if !ok {
+		t.Skip("You must define environment variable %s to run this test", keynuker_go_common.EnvVarKeyNukerTestAwsAccessKeyId)
+	}
+
+	awsSecretAccessKey, ok := os.LookupEnv(keynuker_go_common.EnvVarKeyNukerTestAwsSecretAccessKey)
+	if !ok {
+		t.Skip("You must define environment variable %s to run this test", keynuker_go_common.EnvVarKeyNukerTestAwsSecretAccessKey)
+	}
+
+	targetAwsAccounts := []TargetAwsAccount{
+		{
+			AwsAccessKeyId: awsAccessKeyId,
+			AwsSecretAccessKey: awsSecretAccessKey,
+		},
+	}
+
+	params := ParamsFetchAwsKeys{
+		KeyNukerOrg: "default",
+		TargetAwsAccounts: targetAwsAccounts,
+	}
+
+	doc, err := FetchAwsKeys(params)
+
+	assert.NoError(t, err, "Unexpected Error")
+
+	marshalled, err := json.MarshalIndent(doc, "", "    ")
+
+	log.Printf("FetchedAwsKeys: %v", string(marshalled))
+
+}
