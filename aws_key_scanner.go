202a568b2f8b111572017fbdcc3ff25ba17adc01
Traun Leyden <traun.leyden@gmail.com>
2017-10-05 21:15:46 -0700

    Add trie-based approach that does a single pass scan on the content.  Getting ~2x performance improvement

--- aws_key_scanner.go
+++ aws_key_scanner.go
@@ -5,17 +5,30 @@ package keynuker
 
 import (
 	"regexp"
+
+	"log"
+
+	"github.com/derekparker/trie"
 )
 
+type ScannerState int
 
-// Scan the content to see if any of the aws keys are present
-func Scan(accessKeyMetadata []FetchedAwsAccessKey, content []byte) (leaks []FetchedAwsAccessKey, nearbyContent []byte, err error) {
+const (
+	ScannerStateInToken ScannerState = iota
+	ScannerStateOutsideToken
+)
 
-	for _, keyMetadata := range accessKeyMetadata {
+func Scan(accessKeysToScan []FetchedAwsAccessKey, content []byte) (leaks []FetchedAwsAccessKey, nearbyContent []byte, err error) {
+	return ScanViaRegexLoop(accessKeysToScan, content)
+}
+
+// This is grossly inefficient but it passes all of the tests
+func ScanViaRegexLoop(accessKeysToScan []FetchedAwsAccessKey, content []byte) (leaks []FetchedAwsAccessKey, nearbyContent []byte, err error) {
+
+	for _, keyMetadata := range accessKeysToScan {
 
 		key := *keyMetadata.AccessKeyId
 
-		// TODO: this is grossly inefficient.  Convert to a lexer based approach
 		r, err := regexp.Compile(key)
 		if err != nil {
 			return nil, nil, err
@@ -29,4 +42,130 @@ func Scan(accessKeyMetadata []FetchedAwsAccessKey, content []byte) (leaks []Fetc
 
 	return leaks, nil, nil
 
+}
+
+// Scan the input in a single pass and use a trie prefix match to figure out if any aws keys match.
+// This is approx 2x faster than ScanViaRegexLoop, but it still feels slow.
+// TODO: try using lexmachine and see if it's a lot faster.  Do some post-processing to deal with nested tokens (where one token contains another, which is one of the unit tests)
+func ScanViaTrie(accessKeysToScan []FetchedAwsAccessKey, content []byte) (leaks []FetchedAwsAccessKey, nearbyContent []byte, err error) {
+
+	debug := false
+
+	uniqueLeaks := map[string]FetchedAwsAccessKey{}
+
+	leaks = []FetchedAwsAccessKey{}
+
+	trie := trie.New()
+	exactMatch := map[string]FetchedAwsAccessKey{}
+
+	for _, keyMetadata := range accessKeysToScan {
+
+		// Get the access key id
+		key := *keyMetadata.AccessKeyId
+
+		// Add it to the trie to be indexed
+		trie.Add(key, keyMetadata)
+		exactMatch[key] = keyMetadata
+
+	}
+
+	runes := []rune(string(content))
+
+	currentScannerState := ScannerStateOutsideToken
+
+	scanStartPointer := 0
+	scanEndPointer := 1
+
+	for {
+
+		if scanStartPointer >= len(runes) {
+			break
+		}
+
+		if scanEndPointer > len(runes) {
+			scanEndPointer = len(runes)
+		}
+
+		currentStateName := ""
+		if currentScannerState == ScannerStateInToken {
+			currentStateName = "ScannerStateInToken"
+		} else {
+			currentStateName = "ScannerStateOutsideToken"
+		}
+		if debug {
+			log.Printf("runes[%d:%d] - state: %s", scanStartPointer, scanEndPointer, currentStateName)
+		}
+
+		currentRunes := runes[scanStartPointer:scanEndPointer]
+		if debug {
+			log.Printf("currentRunes: |%v|", string(currentRunes))
+		}
+
+		switch trie.HasKeysWithPrefix(string(currentRunes)) {
+		case true:
+
+			if debug {
+				log.Printf("trie.HasKeysWithPrefix: |%v|.  Switching state -> ScannerStateInToken", string(currentRunes))
+			}
+
+			currentScannerState = ScannerStateInToken
+
+			accessKeyMeta, found := exactMatch[string(currentRunes)]
+			if found {
+				if debug {
+					log.Printf("trie exact match: %v", *accessKeyMeta.AccessKeyId)
+				}
+				uniqueLeaks[string(currentRunes)] = accessKeyMeta
+			}
+
+			// Keep consuming runes by expanding the scanning window to be one rune larger
+			scanEndPointer += 1
+
+		case false:  // trie.HasKeysWithPrefix(string(currentRunes)) == false
+
+			switch currentScannerState {
+			case ScannerStateInToken:
+
+				// There are no prefix matches, and we were in a possible token, so we're out of the token now
+
+				currentScannerState = ScannerStateOutsideToken
+
+				// Suppose the goal is to find these two keys [ROX, OA5]
+				// And the input text is: "ROA5XYZ"
+				// It will be in a token match state for the "RO" since the prefix will match "ROX",
+				// but when it gets to the "A" in "ROA5", the "ROA" prefix will not match anything.
+				// At this point, slide the start of the window one rune to the right so it's now aligned to "OA5"
+				scanStartPointer += 1
+
+			case ScannerStateOutsideToken:
+
+				// There are no prefix matches, and we weren't in a token
+
+				if scanEndPointer > (scanStartPointer + 1) {
+
+					// This can happen if we just recently partially or fully matched a token, and have a wide scanning window,
+					// but then on reading the next rune it doesn't match any tokens, but still has this wide scanning window.
+					// Shrink the window back down until it's reading a single character.  It does do some (probably)
+					// unecessary scanning
+					scanEndPointer -= 1
+
+				} else {
+
+					// Normal case
+					// Slide the scanning window one rune to the right
+					scanStartPointer += 1
+					scanEndPointer += 1
+				}
+
+			}
+
+		}
+
+	}
+
+	for _, accessKeyMeta := range uniqueLeaks {
+		leaks = append(leaks, accessKeyMeta)
+	}
+
+	return leaks, nil, nil
 }
--- aws_key_scanner_test.go
+++ aws_key_scanner_test.go
@@ -7,29 +7,72 @@ import (
 	"fmt"
 	"testing"
 
+	"log"
+	"math/rand"
+	"time"
+
+	"bytes"
+	"io/ioutil"
+
+	"sort"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/couchbaselabs/go.assert"
-	"bytes"
 )
+
+var letterRunes = []rune("123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var numKeys = 350
 
 func TestAwsKeyScanner(t *testing.T) {
 
 	leakedKey := "FakeAccessKey"
+	leakedKey2 := "FakeAccessKey2"
 	accessKeyMetadata := []FetchedAwsAccessKey{
 		{
 			AccessKeyId: aws.String(leakedKey),
 			UserName:    aws.String("fakeuser@test.com"),
 		},
+		{
+			AccessKeyId: aws.String(leakedKey2),
+			UserName:    aws.String("fakeuser2@test.com"),
+		},
 	}
 
 	// Single leaked key
+	log.Printf("--------------------- Single leaked key")
 	eventContent := []byte(fmt.Sprintf(`"patch":"@@ -1,2 +1,2 @@\n \n-Oops I just leaked %s!`, leakedKey))
 	leakedKeys, _, err := Scan(accessKeyMetadata, eventContent)
 	assert.True(t, err == nil)
 	assert.True(t, len(leakedKeys) == 1)
-	assert.True(t, *leakedKeys[0].AccessKeyId == leakedKey)
+	assert.True(t, contains(accessKeyMetadata[0:1], leakedKeys))
+
+	// Two non-adjacent leaked keys
+	log.Printf("--------------------- Two non-adjacent leaked keys")
+	eventContent = []byte(fmt.Sprintf(`"patch":"@@ -1,2 +1,2 @@\n \n-Oops I just leaked %s %s!`, leakedKey, leakedKey2))
+	leakedKeys, _, err = Scan(accessKeyMetadata, eventContent)
+	assert.True(t, err == nil)
+	assert.True(t, len(leakedKeys) == 2)
+	assert.True(t, contains(accessKeyMetadata, leakedKeys))
+
+	// Two non-adjacent leaked keys
+	log.Printf("--------------------- Two adjacent leaked keys")
+	eventContent = []byte(fmt.Sprintf(`"patch":"@@ -1,2 +1,2 @@\n \n-Oops I just leaked %s%s!`, leakedKey, leakedKey2))
+	leakedKeys, _, err = Scan(accessKeyMetadata, eventContent)
+	assert.True(t, err == nil)
+	assert.True(t, len(leakedKeys) == 2)
+	assert.True(t, contains(accessKeyMetadata, leakedKeys))
+
+	// Two leaked keys, leaked twice each -- should de-dupe
+	log.Printf("--------------------- Two leaked keys, leaked twice each -- should de-dupe")
+	eventContent = []byte(fmt.Sprintf(`"patch":"@@ -1,2 +1,2 @@\n \n-Oops I just leaked %s%s and again %s%s!`,
+		leakedKey, leakedKey2, leakedKey2, leakedKey))
+	leakedKeys, _, err = Scan(accessKeyMetadata, eventContent)
+	assert.True(t, err == nil)
+	assert.True(t, len(leakedKeys) == 2)
+	assert.True(t, contains(accessKeyMetadata, leakedKeys))
 
 	// No leaked key
+	log.Printf("--------------------- No leaked key")
 	eventContent = []byte(fmt.Sprintf(`"patch":"@@ -1,2 +1,2 @@\n \n- I'm not a leaker'`))
 	noLeakedKeys, _, err2 := Scan(accessKeyMetadata, eventContent)
 	assert.True(t, err2 == nil)
@@ -37,36 +80,225 @@ func TestAwsKeyScanner(t *testing.T) {
 
 }
 
-func BenchmarkAwsKeyScanner(b *testing.B) {
+func TestAwsKeyScannerOverlappingKeys(t *testing.T) {
 
-	// Setup keys and content
-	leakedKeyPrefix := "FakeAccessKey"
-	accessKeyMetadata := []FetchedAwsAccessKey{}
-
-	contentBuffer := bytes.NewBufferString("")
-
-	numIterations := 1000
-	for i := 0; i < numIterations; i++ {
-		leakedKey := fmt.Sprintf("%s-%d", i, leakedKeyPrefix)
-		fetchedAwsAccessKey := FetchedAwsAccessKey{
-			AccessKeyId: aws.String(leakedKey),
+	leakedKeyInner := "FakeAccessKey"
+	leakedKeyOuter := "OuterFakeAccessKeyOuter"
+	accessKeyMetadata := []FetchedAwsAccessKey{
+		{
+			AccessKeyId: aws.String(leakedKeyInner),
 			UserName:    aws.String("fakeuser@test.com"),
-		}
-		accessKeyMetadata = append(accessKeyMetadata, fetchedAwsAccessKey)
-
-		contentBuffer.WriteString(fmt.Sprintf(`"patch":"@@ -1,2 +1,2 @@\n \n-Oops I just leaked %s! `, leakedKey))
+		},
+		{
+			AccessKeyId: aws.String(leakedKeyOuter),
+			UserName:    aws.String("fakeuser2@test.com"),
+		},
 	}
 
-	eventContent := contentBuffer.Bytes()
+	// Leak the outer key, expect to find both
+	eventContent := []byte(fmt.Sprintf(`"patch":"@@ -1,2 +1,2 @@\n \n-Oops I just leaked %s!`, leakedKeyOuter))
+	leakedKeys, _, err := Scan(accessKeyMetadata, eventContent)
+	assert.True(t, err == nil)
+	assert.True(t, len(leakedKeys) == 2)
 
+	assert.True(t, contains(accessKeyMetadata, leakedKeys))
+
+}
+
+func contains(expected, actual []FetchedAwsAccessKey) bool {
+
+	if len(expected) != len(actual) {
+		return false
+	}
+
+	actualKeys := []string{}
+	expectedKeys := []string{}
+
+	for i, fetchedAwsAccessKey := range actual {
+		actualKeys = append(actualKeys, *fetchedAwsAccessKey.AccessKeyId)
+		expectedKeys = append(expectedKeys, *expected[i].AccessKeyId)
+	}
+
+	sort.Strings(actualKeys)
+	sort.Strings(expectedKeys)
+
+	for i, expectedKey := range expectedKeys {
+		actualKey := actualKeys[i]
+		if expectedKey != actualKey {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestAwsKeyScannerLargeFileManyKeys(t *testing.T) {
+
+	largeFileWithKeys, accessKeyMetadata, err := largeFileContentAndKeys()
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+
+	leaks, _, err := Scan(accessKeyMetadata, largeFileWithKeys)
+
+	if len(leaks) != numKeys {
+		uniqueKeys := map[string]string{}
+		for _, accessKey := range accessKeyMetadata {
+			uniqueKeys[*accessKey.AccessKeyId] = *accessKey.AccessKeyId
+		}
+		for _, leak := range leaks {
+			_, hasKey := uniqueKeys[*leak.AccessKeyId]
+			if hasKey {
+				delete(uniqueKeys, *leak.AccessKeyId)
+			}
+		}
+		log.Printf("Undetected leaks")
+		for key, _ := range uniqueKeys {
+			log.Printf("Undetected key: %v", key)
+		}
+	}
+
+	assert.Equals(t, len(leaks), numKeys)
+}
+
+func TestAwsKeyScannerProblematicTest(t *testing.T) {
+
+	problematicTest, err := Asset("testdata/ProblematicText.txt")
+	if err != nil {
+		t.Fatalf("Err: %v", err)
+	}
+
+	accessKeyMetadata := []FetchedAwsAccessKey{}
+	fetchedAwsAccessKey := FetchedAwsAccessKey{
+		AccessKeyId: aws.String("OAQWO93BD58KJNGGXRF1E2VY9A6599OFMSGWU34OU-205"),
+		UserName:    aws.String("fakeuser@test.com"),
+	}
+	accessKeyMetadata = append(accessKeyMetadata, fetchedAwsAccessKey)
+
+	fetchedAwsAccessKey2 := FetchedAwsAccessKey{
+		AccessKeyId: aws.String("ROA5IUM5ZFWZC85PP11C8XCICWW35PPB888WV6ABKE1HM2FNV2ORWKPCV5SHC4KIE8BLC131G8RS437OPF68SYM6T2XCGTZRF2AM24T2SP-202"),
+		UserName:    aws.String("fakeuser@test.com"),
+	}
+	accessKeyMetadata = append(accessKeyMetadata, fetchedAwsAccessKey2)
+
+	leaks, _, err := Scan(accessKeyMetadata, problematicTest)
+	assert.Equals(t, len(leaks), 1)
+
+}
+
+func BenchmarkAwsKeyScanner(b *testing.B) {
+
+	largeFileWithKeys, accessKeyMetadata, err := largeFileContentAndKeys()
+	if err != nil {
+		b.Fatalf("Error: %v", err)
+	}
 	// Reset benchmark timer so that the results aren't skewed by the time taken during setup
 	b.ResetTimer()
 
 	for n := 0; n < b.N; n++ {
-		_, _, err := Scan(accessKeyMetadata, eventContent)
+		now := time.Now()
+		_, _, err := Scan(accessKeyMetadata, largeFileWithKeys)
+		delta := time.Since(now)
+		log.Printf("Scanned content in %v", delta)
 		if err != nil {
 			b.Fatalf("Unexpected error scanning content.  Err: %v", err)
 		}
 
 	}
+}
+
+// Inject the keys into the content and return it
+func injectKeys(content []byte, accessKeys []string) []byte {
+
+	if len(content) < len(accessKeys) {
+		panic(fmt.Sprintf("len(content) < len(accessKeys)"))
+	}
+
+	// If we have more content bytes than keys (usual case), interject the keys
+	// roughly evenly within the content
+
+	// Make a new slice that will be returned, which has enough capacity
+	// First figure out how much content the accessKeys slice contains.
+	accessKeysRunes := 0
+	for _, accessKey := range accessKeys {
+		accessKeysRunes += len(accessKey)
+	}
+
+	contentPointer := 0
+
+	resultContent := bytes.NewBufferString("")
+
+	// Find the "intersperse" distance between normal content and keys to be injected
+	intersperseBytes := len(content) / len(accessKeys)
+
+	for _, accessKey := range accessKeys {
+
+		resultContent.WriteString(accessKey)
+		endRange := contentPointer + intersperseBytes
+		if endRange > len(content) {
+			endRange = len(content) - 1
+		}
+		resultContent.Write(content[contentPointer:endRange])
+		contentPointer += intersperseBytes
+	}
+
+	return resultContent.Bytes()
+
+}
+
+func largeFileContentAndKeys() (contentWithKeys []byte, accessKeyMetadata []FetchedAwsAccessKey, err error) {
+
+	accessKeyMetadata = []FetchedAwsAccessKey{}
+	accessKeys := []string{}
+
+	uniqueKeys := map[string]string{}
+
+	for i := 0; i < numKeys; i++ {
+		keyLength := RandIntRange(4, 128)
+		leakedKeyPrefix := RandStringRunes(keyLength)
+		leakedKey := fmt.Sprintf("%s-%d", leakedKeyPrefix, i)
+		// log.Printf("leakedKey: %v", leakedKey)
+		fetchedAwsAccessKey := FetchedAwsAccessKey{
+			AccessKeyId: aws.String(leakedKey),
+			UserName:    aws.String("fakeuser@test.com"),
+		}
+		accessKeyMetadata = append(accessKeyMetadata, fetchedAwsAccessKey)
+		accessKeys = append(accessKeys, leakedKey)
+		_, hasKey := uniqueKeys[leakedKey]
+		if hasKey {
+			return nil, nil, fmt.Errorf("Tried to add duplicate key: %v", leakedKey)
+		}
+		uniqueKeys[leakedKey] = leakedKey
+
+	}
+
+	largeFile, err := Asset("testdata/largefile.txt")
+	if err != nil {
+		return nil, nil, fmt.Errorf("Unable to load large commit asset: %v", err)
+	}
+
+	// Multiply large file to take it from 1 MB -> 10 MB
+	multipliedLargeFile := bytes.NewBuffer(largeFile)
+	for i := 0; i < 10; i++ { // TEMP SET to 1.  Reset to 10 for benchmarking
+		multipliedLargeFile.Write(largeFile)
+	}
+
+	contentWithKeys = injectKeys(multipliedLargeFile.Bytes(), accessKeys)
+
+	ioutil.WriteFile("/tmp/injectedKeys", contentWithKeys, 0644)
+
+	return contentWithKeys, accessKeyMetadata, nil
+
+}
+
+func RandIntRange(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
