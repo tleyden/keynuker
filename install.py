82473276015b1647f098cc42728dab2a0d2d597c
Traun Leyden <traun.leyden@gmail.com>
2017-10-13 07:37:22 -0700

    Fix install errors

--- install.py
+++ install.py
@@ -458,13 +458,13 @@ def openwhisk_action_exists(openwhisk_action):
     return subprocess.call(command, shell=True) == 0
 
 def openwhisk_trigger_exists(openwhisk_trigger):
-    command = "wsk trigger get {} parameters".format(
+    command = "wsk trigger get {}".format(
         openwhisk_trigger,
     )
     return subprocess.call(command, shell=True) == 0
 
 def openwhisk_rule_exists(openwhisk_rule):
-    command = "wsk rule get {} parameters".format(
+    command = "wsk rule get {}".format(
         openwhisk_rule,
     )
     return subprocess.call(command, shell=True) == 0
