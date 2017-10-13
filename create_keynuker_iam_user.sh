7e87b8134edd98d862ecd35dab490c38337c47cb
Traun Leyden <traun.leyden@gmail.com>
2017-10-12 20:45:53 -0700

    Refine script

--- create_keynuker_iam_user.sh
+++ create_keynuker_iam_user.sh
@@ -7,10 +7,17 @@ if [ -z "$AWS_PROFILE" ]; then
 fi
 
 wget https://raw.githubusercontent.com/tleyden/keynuker/master/docs/keynuker-iam-user-policy.yml
-echo "Installing Cloudformation Template with IAM User + Policy + Access key"
+echo "Installing Cloudformation Template with IAM User with policy:"
 cat keynuker-iam-user-policy.yml
-aws cloudformation create-stack --stack-name KeyNukerIAMAccess --template-body file://keynuker-iam-user-policy.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --profile $AWS_PROFILE && \
+aws cloudformation create-stack --stack-name KeyNukerIAMAccess --template-body file://keynuker-iam-user-policy.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --profile $AWS_PROFILE
+
+echo "Waiting for cloudformation (I'll sleep for 60 seconds -- you go check your email and we'll see you back here in two hours)..."
+
+sleep 60
+
+echo "Here are the AWS Access Key and Secret Access keys that were generated: "
+
 aws cloudformation describe-stacks --stack-name KeyNukerIAMAccess --query 'Stacks[0].Outputs[?OutputKey==`AwsAccessKey`].OutputValue' --output text --profile $AWS_PROFILE && \
-aws cloudformation describe-stacks --stack-name KeyNukerIAMAccess --query 'Stacks[0].Outputs[?OutputKey==`AwsSecretAccessKey`].OutputValue' --output text --profile $AWS_PROFILE
+aws cloudformation describe-stacks --stack-name KeyNukerIAMAccess --query 'Stacks[0].Outputs[?OutputKey==`AwsSecretAccessKey`].OutputValue' --output text --profile $AWS_PROFILE && \
 
 echo "Success!  You now have a Cloudformation named KeyNukerIAMAccess and an IAM user named KeyNuker with an associated policy that allows ListUsers, ListKeys, DeleteKeys.  To uninstall, delete the KeyNukerIAMAccess Cloudformation from the Web UI or via the CLI"
